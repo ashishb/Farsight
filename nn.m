9671a1e009cbcd4b6aeae17e766769df3c71d6ae
Ashish Bhatia <ashishbhatia.ab@gmail.com>
2012-12-09 22:18:53 -0800

    Added code for Neural Networks (it is giving out of memory error currently).

---
+++ nn.m
@@ -0,0 +1,21 @@
+tic()
+[sparseMatrix, tokenlist, category] = readMatrix('data/matrix');
+category = sign(category - 2.5);
+toc()
+
+trainMatrix = sparseMatrix(:,:);
+trainCategory = category(:)';
+
+siz = 6000;
+for m = 1:500:siz
+  disp 'Starting training for m=', m
+  x = trainMatrix(1:m, :)';
+  t = trainCategory(1:m);
+  % Train
+  net = feedforwardnet(10);
+  net = train(net, x, t);
+  view(net);
+  y = sim(net, x);
+  perf = perform(net, y, t);
+  disp 'Done traing for m=', m
+end
